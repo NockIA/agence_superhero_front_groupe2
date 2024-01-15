fc0dff542e42a1cfced9a8a8c3e5fe574149c5a0
ckevin <ISeeNock.Draw@gmail.com>
2024-01-15 17:35:52 +0100

    :truck: | import from previous repo

---
+++ agence_superhero_front/src/pages/Auth/signin_page.tsx
@@ -0,0 +1,13 @@
+import { SignIn } from "../../components/Auth/SignIn/signIn"
+import NavigationBar from "../../components/Nav/nav";
+
+const SignInPage = () => {
+    return (
+        <>
+         <NavigationBar/>
+         <SignIn/>
+        </>
+    )
+}
+
+export default SignInPage;
---
+++ agence_superhero_front/src/pages/Auth/signup_page.tsx
@@ -0,0 +1,13 @@
+import { SignUp } from "../../components/Auth/SignUp/signUp";
+import NavigationBar from "../../components/Nav/nav";
+
+const SignUpPage = () => {
+    return (
+        <>
+        <NavigationBar/>
+        <SignUp/>
+        </>
+    )
+}
+
+export default SignUpPage;

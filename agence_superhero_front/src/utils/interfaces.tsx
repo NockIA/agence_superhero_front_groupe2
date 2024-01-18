3e9a9568f8722f0b1d081ab96cbf458f2a127469
ckevin <ISeeNock.Draw@gmail.com>
2024-01-18 23:51:12 +0100

    :art: | split constants in diff files

---
+++ agence_superhero_front/src/utils/interfaces.tsx
@@ -0,0 +1,29 @@
+
+export interface PowersProps {
+  id: string;
+  name: string;
+  linkImage: string;
+}
+
+export interface GadgetsProps {
+  id: string;
+  name: string;
+  linkImage: string;
+}
+
+export interface VehicleProps {
+  id: string;
+  name: string;
+  linkImage: string;
+  description: string;
+}
+
+export interface PlanetProps {
+  id: string;
+  name: string;
+  linkImage: string;
+  description: string;
+}
+
+
+
